667d3c89dc6397d65c32f98eddb03961490446bc
exefer <jessieodeh08@gmail.com>
2026-08-28 15:40:32 +0200

    feat(nvim): add crates and package-info
    Wire c and n maps through which-key.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,6 +13,10 @@ vim.pack.add({
 	"https://github.com/saghen/blink.lib",
 	"https://github.com/saghen/blink.cmp",
 	"https://github.com/stevearc/conform.nvim",
+	"https://github.com/folke/which-key.nvim",
+	"https://github.com/saecki/crates.nvim",
+	"https://github.com/MunifTanjim/nui.nvim",
+	"https://github.com/vuki656/package-info.nvim",
 	"https://github.com/vyfor/cord.nvim",
 	"https://github.com/navarasu/onedark.nvim",
 })
@@ -32,6 +36,25 @@ require("tiny-inline-diagnostic").setup({
 		transparent_bg = true,
 	},
 })
+
+-- dependency management
+local crates = require("crates")
+crates.setup({
+	completion = {
+		crates = {
+			enabled = true,
+		},
+	},
+	lsp = {
+		enabled = true,
+		actions = true,
+		completion = true,
+		hover = true,
+	},
+})
+
+local pkg_info = require("package-info")
+pkg_info.setup()
 
 -- formatting
 require("conform").setup({
@@ -74,6 +97,35 @@ vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("ts_ls")
 
 -- keymaps
-vim.keymap.set("n", "<leader>ih", function()
-	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-end, { desc = "Toggle inlay hints" })
+local wk = require("which-key")
+wk.setup({
+	preset = "classic",
+	delay = 200,
+})
+
+wk.add({
+	{
+		"<leader>ih",
+		function()
+			vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+		end,
+		desc = "Toggle inlay hints",
+	},
+})
+
+wk.add({
+	{ "<leader>c", group = "crates" },
+	{ "<leader>n", group = "npm" },
+	{ "<leader>nu", pkg_info.update, desc = "Update package on line" },
+	{ "<leader>nd", pkg_info.delete, desc = "Delete package on line" },
+	{ "<leader>ni", pkg_info.install, desc = "Install new package" },
+	{ "<leader>np", pkg_info.change_version, desc = "Change package version" },
+	{ "<leader>cf", crates.show_features_popup, desc = "Show crate features" },
+	{ "<leader>cd", crates.show_dependencies_popup, desc = "Show crate dependencies" },
+	{ "<leader>cu", crates.update_crate, desc = "Update crate", mode = "n" },
+	{ "<leader>cu", crates.update_crates, desc = "Update crates", mode = "v" },
+	{ "<leader>ca", crates.update_all_crates, desc = "Update all crates" },
+	{ "<leader>cU", crates.upgrade_crate, desc = "Upgrade crate", mode = "n" },
+	{ "<leader>cU", crates.upgrade_crates, desc = "Upgrade crates", mode = "v" },
+	{ "<leader>cA", crates.upgrade_all_crates, desc = "Upgrade all crates" },
+})
